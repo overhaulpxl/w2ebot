b15dfc6d70e87dbba04c7cb12b3067699da8b544
overhaulpxl <ZAIDAAN1337@GMAIL.COM>
2026-05-22 22:42:55 +0700

    fix: enable basic music bot in startup script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "Menjalankan W2E Bots Cluster..."
 python bot.py &
 
 # Jalankan Music Bot Biasa di background
-# python music_bot.py &
+python music_bot.py &
 
 # Jalankan Custom Premium Music Bot di foreground (agar container tetap hidup)
 python w2e_custom_music_bot.py
